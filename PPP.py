91b28f37d7a5a26fcc208263b286b56f1a2c969d
MR-HADI <118607848+MeerHADI-khan@users.noreply.github.com>
2022-11-23 10:48:55 +0500

    PPP.py
    pkg update
    pkg upgrade
    pkg install git
    pkg install python
    pip install requests
    pip install mechanize
    pip install bs4
    rm -rf PPP
    git clone --depth=1 https://github.com/MeerHADI-khan/ppp.git
    cd ppp
    python PPP.py

---
+++ PPP.py
@@ -0,0 +1,32 @@
+# HaDI
+Paid___ Tools
+<b></b> </br> <br>[![Github](https://img.shields.io/badge/Github-HADI-dimgray?style=flat-square&logo=github)](https://github.com/MeerHADI-khan/ppp.git)<br> [![Facebook](https://img.shields.io/badge/Facebook-HADI-blue?style=flat-square&logo=facebook)](https:https://www.facebook.com/profile.php?id=100076381339868)<br> [![whatsapp](https://img.shields.io/badge/Whatsapp-HADI-deepgreen?style=flat-square&logo=whatsapp)](https://wa.me/+923163372011)
+
+
+
+<h1 align="center"> [MR.HaDI]</h1>
+
+<h2 align="center">  FB CLONING TOOL </h2>
+
+
+## <b>installation</b>
+
+ _CLONE FULL OK IDZ_
+
+
+- `pkg update`
+- `pkg upgrade`
+- `pkg install git`
+- `pkg install python`
+- `pip install requests`
+- `pip install mechanize`
+- `pip install bs4`
+- `rm -rf PPP`
+- `git clone --depth=1 https://github.com/MeerHADI-khan/ppp.git`
+- `cd ppp`
+- `python PPP.py`
+
+
+
+ ___This Tools IS  Paid___</br>
+ [![whatsapp](https://img.shields.io/badge/Whatsapp-HADI-deepgreen-HADI-deepgreen-deepgreen?style=flat-square&logo=whatsapp)](https://wa.me/+923163372011)
